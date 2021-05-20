70e5f48570714683385d6e48aaa636011963144d
Chris McGivney <cmmcgivney84@gmail.com>
2021-05-20 10:17:22 -0600

    third time is a charm

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  DATABASE_URL='mongodb+srv://hack-a-thon-portfolio:hack-a-thon-portfolio@cluster0.1d541.mongodb.net/hack-portfolio?retryWrites=true&w=majority',
-  SECRET_KEY='secret key'
+  DATABASE_URL:'mongodb+srv://hack-a-thon-portfolio:hack-a-thon-portfolio@cluster0.1d541.mongodb.net/hack-portfolio?retryWrites=true&w=majority',
+  SECRET_KEY:'secret key'
 };
