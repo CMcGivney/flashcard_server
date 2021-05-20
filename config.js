1af4e1251057a5e133e1ae83fde0f36cfe451f36
Chris McGivney <cmmcgivney84@gmail.com>
2021-05-20 10:09:52 -0600

    update: config

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  DATABASE_URL='mongodb+srv://hack-a-thon-portfolio:hack-a-thon-portfolio@cluster0.1d541.mongodb.net/hack-portfolio?retryWrites=true&w=majority'
+  SECRET_KEY='secret key'
+};
--- index.js
+++ index.js
@@ -2,14 +2,13 @@ require("dotenv").config();
 const { ApolloServer, PubSub } = require("apollo-server");
 const mongoose = require("mongoose");
 
-
 const typeDefs = require("./graphql/typeDefs");
 const resolvers = require('./graphql/resolvers')
 const { MONGODB } = require('./config.js');
 
 const pubsub = new PubSub()
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
