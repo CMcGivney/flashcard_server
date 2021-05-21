182cd40fdeed59ae06d2597386f2164a6da9f33e
Chris McGivney <cmmcgivney84@gmail.com>
2021-05-20 21:51:39 -0600

    removed config vars

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  MONGODB:'mongodb+srv://hack-a-thon-portfolio:hack-a-thon-portfolio@cluster0.1d541.mongodb.net/hack-portfolio?retryWrites=true&w=majority',
-  SECRET_KEY:'secret key'
-};
